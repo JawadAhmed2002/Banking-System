b76469c1126636ca452b6d9db2734a462cf166a0
JawadAhmed2002 <jawad.kohat2002@gmail.com>
2022-07-19 14:58:51 +0500

    add secret key

--- banking_system/__init__.py
+++ banking_system/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
 import os
-app = Flask(__name__)
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
 app.secret_key = environ.get('SECRET_KEY')
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/banking_system'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SALT'] = environ.get('SALT')
